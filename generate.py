50abd509def750ce68015913f52cf7c6c8be01c1
Ed Baker <edwab@zoomac-c02k70uxdnmp.nhm.ac.uk>
2016-06-17 12:37:36 +0100

    Generate combined output.

---
+++ generate.py
@@ -0,0 +1,26 @@
+import glob
+import urllib
+
+sources = [
+	'http://phasmida.myspecies.info/interactions.txt',
+	'http://phthiraptera.myspecies.info/interactions.txt'
+	]
+
+i = 0;
+for source in sources:
+	dl = urllib.URLopener();
+	dl.retrieve(source, 'files/'+str(i)+'.csv')
+	i += 1
+
+dl_files = glob.glob("files/*.csv") 
+
+header_saved = False
+with open('output.csv','wb') as fout:
+    for filename in dl_files:
+        with open(filename) as fin:
+            header = next(fin)
+            if not header_saved:
+                fout.write(header)
+                header_saved = True
+            for line in fin:
+                fout.write(line)
